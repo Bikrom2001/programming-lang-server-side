704ea4ce7d2c5a1db618c7074c1fb5fbb8db1cea
Bikrom-Roy <bikromroy2001@gmail.com>
2022-10-26 19:09:40 +0600

    server start

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+const Port = process.env.Port || 5000;
+
+
+
+
+
+
+app.listen(Port, () => {
+    console.log('server is running', Port);
+})
